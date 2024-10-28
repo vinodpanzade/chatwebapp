641ea837d89b4ec68d273dc672cd870c288146de
Vinod Panzade <161502332+vinodpanzade@users.noreply.github.com>
2024-10-28 20:22:24 +0530

    Update vercel.js

--- Frontend/vercel.js
+++ Frontend/vercel.js
@@ -1,5 +1,26 @@
 {
-   "rewrites":  [
-       {"source": "/(.*)", "destination": "/"}
+    "version": 2,
+    "builds": [
+        {
+            "src": "frontend/package.json",
+            "use": "@vercel/static-build",
+            "config": {
+                "distDir": "frontend/dist"
+            }
+        },
+        {
+            "src": "server/index.js", // Adjust path to your server entry point
+            "use": "@vercel/node"
+        }
+    ],
+    "routes": [
+        {
+            "src": "/api/(.*)",
+            "dest": "/server/index.js" // Adjust to your server entry point
+        },
+        {
+            "src": "/(.*)",
+            "dest": "/frontend/dist/$1"
+        }
     ]
 }
